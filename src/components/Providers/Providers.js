68c43bd25a567b06f8d78ec93dd2b08dd07f431c
Hamza Alvi <hamzaalvi224@gmail.com>
2023-05-11 09:12:49 +0500

    configure prisma and redux toolkit

--- src/components/Providers/Providers.js
+++ src/components/Providers/Providers.js
@@ -1,13 +1,16 @@
 "use client";
-
+import { Provider } from "react-redux";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "@/styles/theme";
+import { store } from "@/store";
 
 function Providers({ children }) {
   return (
-    <ChakraProvider resetCSS={true} theme={theme}>
-      {children}
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider resetCSS={true} theme={theme}>
+        {children}
+      </ChakraProvider>
+    </Provider>
   );
 }
 
