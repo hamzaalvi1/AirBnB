49b07f0430db2c8ee62f6b30b290e8caef62ab27
Hamza Alvi <hamzaalvi224@gmail.com>
2023-05-12 23:27:26 +0500

    Add validation in create user route

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -3,11 +3,24 @@ import bcrypt from "bcrypt";
 
 import prisma from "@/libs/prismadb";
 
+const checkEmailExist = async (email) => {
+  const user = await prisma.user.findUnique({ where: { email } });
+  return !!user;
+};
+
 export async function POST(request) {
   const body = await request.json();
 
   const { email, password, name } = body;
   const hashedPassword = await bcrypt.hash(password, 12);
+
+  const isEmailExist = await checkEmailExist(email);
+  if (isEmailExist) {
+    return NextResponse.json({
+      status: "403",
+      message: "Email already exists",
+    });
+  }
 
   const user = await prisma.user.create({
     data: {
