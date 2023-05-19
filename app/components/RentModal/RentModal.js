992130ccdd5ef991b7085562a0eba3d42a1cde30
hamza.ahmed@koderlabs.com <hamzaalvi224@gmail.com>
2023-05-20 00:43:19 +0500

    formik integration with rent modal WIP

--- app/components/RentModal/RentModal.js
+++ app/components/RentModal/RentModal.js
@@ -8,17 +8,51 @@ const RentModal = (props) => {
   const { isOpen, onClose, title } = props;
   const [rentStepper, setRentStepper] = useState(0);
 
+  const initialValues = {
+    category: "",
+    location: "",
+    info: "",
+    images: "",
+    description: "",
+    price: "",
+  };
+
   return (
     <Modal
       isOpen={isOpen}
       onClose={onClose}
-      title={title}
       size={"lg"}
       styleProps={{ borderRadius: "md" }}
+      title={title}
     >
-      <RentStepper step={rentStepper} />
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {(formikProps) => {
+          const { setSubmitting, handleSubmit, setFieldValue, setFieldError } =
+            formikProps;
+
+          const handleValuesChange = (type, value) =>
+            setFieldValue(type, value);
+
+          const handleErrorOccured = (fieldName, error) =>
+            setFieldError(fieldName, error);
+
+          return (
+            <form onSubmit={handleSubmit}>
+              <RentStepper
+                step={rentStepper}
+                handleValuesChange={handleValuesChange}
+              />
+            </form>
+          );
+        }}
+      </Formik>
     </Modal>
   );
 };
 
-export default RentModal;
+export default withFormik(RentModal);
