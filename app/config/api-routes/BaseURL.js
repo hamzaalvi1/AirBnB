87f67912acd13f2526bfd4e625897701fc3c7a1c
hamza.ahmed@koderlabs.com <hamzaalvi224@gmail.com>
2023-07-10 20:20:08 +0500

    Add minor changes

--- app/config/api-routes/BaseURL.js
+++ app/config/api-routes/BaseURL.js
@@ -1,4 +1,6 @@
-const BASE_URL = "https://hotel-reservations.vercel.app/api";
+// const BASE_URL = "https://hotel-reservations.vercel.app/api";
+const BASE_URL = "http://localhost:3000/api";
+
 // "http://localhost:3000/"
 
 export default BASE_URL;
