aafd8c519b0f6a99cdaf147661f398ad54019bff
hamza.ahmed@koderlabs.com <hamzaalvi224@gmail.com>
2023-07-10 21:18:49 +0500

    change auth to pages directory

--- app/actions/getSession.js
+++ app/actions/getSession.js
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 export default async function getSession() {
   return await getServerSession(authOptions);
